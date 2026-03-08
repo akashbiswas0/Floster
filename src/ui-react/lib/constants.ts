251a23140716a79732d6fa24970d87cbec3838c6
akashbiswas0 <akash8860biswas@gmail.com>
2026-03-08 20:08:16 +0530

    feat: add Topbar component and constants

---
+++ src/ui-react/lib/constants.ts
@@ -0,0 +1,10 @@
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+export const SEPOLIA_RPC_URL = 'https://ethereum-sepolia-rpc.publicnode.com'
+export const SEPOLIA_CHAIN_NAME = 'ethereum-testnet-sepolia'
+export const SEPOLIA_SIMULATION_RECEIVER = '0x1729388a37eDC095c17C381fbe43Fb7EbeC44499'
+export const SEPOLIA_PRODUCTION_RECEIVER = '0x7578EbC461DfBC93c2e5Ce93a55163D5fD7D1c91'
+export const DEFAULT_TOKEN_ADDRESS = '0xec4d762FcDCBAa1f9b37760DEe12F508c3F6b53E'
+export const DEFAULT_TOKEN_DECIMALS = 6
+export const DEFAULT_RECIPIENT_ADDRESS = ''
+
+export const TRIGGER_TYPES = ['cron', 'http', 'evmLog'] as const
